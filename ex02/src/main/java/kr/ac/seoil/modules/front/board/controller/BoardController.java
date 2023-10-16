d7abf3a079e7f6ae6e307ab5c7f1c1beb2bf0f7c
seoil <seoil@BY210-G-16>
2023-10-16 18:41:11 +0900

    commit test

--- ex02/src/main/java/kr/ac/seoil/modules/front/board/controller/BoardController.java
+++ ex02/src/main/java/kr/ac/seoil/modules/front/board/controller/BoardController.java
@@ -1,11 +1,14 @@
 package kr.ac.seoil.modules.front.board.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.ac.seoil.modules.front.board.service.BoardService;
+import kr.ac.seoil.modules.front.board.vo.BoardVO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
 
@@ -17,7 +20,9 @@ public class BoardController {
 	private final BoardService boardservice;
 	
 	@GetMapping("/list.do")
-	public void list(Model model) { 
+	public void list(Model model) throws Exception { 
+		List<BoardVO> list = boardservice.getList();
 		
+		model.addAttribute("list", list);
 	}
 }
